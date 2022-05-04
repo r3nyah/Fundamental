040f0f3ceb46af23c67e58bb4321fe77398a3991
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-04 09:33:11 +0700

    Lesson/7

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-        title: Text('First Test'),
-        centerTitle: true,
-        backgroundColor: Colors.purple[700]
-    ),
-    body: Center(
-      child: Text(
-        'Hello Wolld!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.red[600],
-      onPressed: () {},
-      child: Text('click'),
-    ),
-  ),
+  home :Hero()
 ));
+class Hero extends StatelessWidget {
+  const Hero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        appBar: AppBar(
+            title: Text('Second Test'),
+            centerTitle: true,
+            backgroundColor: Colors.purple[700]
+        ),
+        body: Center(
+          child: Text(
+            'Hello Wolld!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[600],
+          onPressed: () {},
+          child: Text('click'),
+        ),
+      );
+  }
+}
+
